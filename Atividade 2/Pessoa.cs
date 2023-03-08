f51fbabfd700cbf4cb58215a103cf7c1d47fe350
Rafa-ListeM <rafaeliturci098@gmail.com>
2023-03-08 10:32:13 -0300

    Criei a primeira pessoa como p1

--- Atividade 2/Pessoa.cs
+++ Atividade 2/Pessoa.cs
@@ -8,5 +8,14 @@ namespace Atividade_2
 {
     internal class Pessoa
     {
-    }
+        private int Id;
+        private string Nome;
+        private string Telefeone;
+
+        public Pessoa(int id, string nome, string telefone);
+        {
+        }
+
+
+}
 }
--- Atividade 2/Program.cs
+++ Atividade 2/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Atividade_2;
+
 Console.WriteLine("Hello, World!");
+
+Pessoa p1 = new Pessoa(1, "Maria", 98770144533M);
+Console.WriteLine()
